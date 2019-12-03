1a470c0f952533ab44120fdb59eb628c987f6a7d
skranbets <38116980+skranbets@users.noreply.github.com>
2019-12-02 22:46:27 -0500

    Added Layout files

--- TDingB51A04/app/src/main/java/com/example/tdingb51a04/FilterListViewHandler.java
+++ TDingB51A04/app/src/main/java/com/example/tdingb51a04/FilterListViewHandler.java
@@ -1,4 +1,39 @@
 package com.example.tdingb51a04;
 
+import android.widget.CheckBox;
+import android.widget.TextView;
+
 public class FilterListViewHandler {
+    private CheckBox checkBox;
+    private TextView textView;
+
+    public FilterListViewHandler()
+    {
+    }
+
+    public FilterListViewHandler(TextView textView, CheckBox checkBox)
+    {
+        this.checkBox = checkBox;
+        this.textView = textView;
+    }
+
+    public CheckBox getCheckBox()
+    {
+        return checkBox;
+    }
+
+    public void setCheckBox(CheckBox checkBox)
+    {
+        this.checkBox = checkBox;
+    }
+
+    public TextView getTextView()
+    {
+        return textView;
+    }
+
+    public void setTextView(TextView textView)
+    {
+        this.textView = textView;
+    }
 }
